86faaf676f76ea147b89233e096b726b2e86a238
Konrad Borowski <konrad@borowski.pw>
2018-04-16 21:21:06 +0200

    Remove usages of deprecated futures .boxed() method

--- src/client/builder.rs
+++ src/client/builder.rs
@@ -552,7 +552,7 @@ impl<'u> ClientBuilder<'u> {
 		// connect to the tcp stream
 		let tcp_stream = match self.async_tcpstream(None, handle) {
 			Ok(t) => t,
-			Err(e) => return future::err(e).boxed(),
+			Err(e) => return Box::new(future::err(e)),
 		};
 
 		let builder = ClientBuilder {
@@ -569,7 +569,7 @@ impl<'u> ClientBuilder<'u> {
 			let (host, connector) = {
 				match builder.extract_host_ssl_conn(ssl_config) {
 					Ok((h, conn)) => (h.to_string(), conn),
-					Err(e) => return future::err(e).boxed(),
+					Err(e) => return Box::new(future::err(e)),
 				}
 			};
 			// secure connection, wrap with ssl
@@ -638,14 +638,14 @@ impl<'u> ClientBuilder<'u> {
 		// connect to the tcp stream
 		let tcp_stream = match self.async_tcpstream(Some(true), handle) {
 			Ok(t) => t,
-			Err(e) => return future::err(e).boxed(),
+			Err(e) => return Box::new(future::err(e)),
 		};
 
 		// configure the tls connection
 		let (host, connector) = {
 			match self.extract_host_ssl_conn(ssl_config) {
 				Ok((h, conn)) => (h.to_string(), conn),
-				Err(e) => return future::err(e).boxed(),
+				Err(e) => return Box::new(future::err(e)),
 			}
 		};
 
@@ -705,7 +705,7 @@ impl<'u> ClientBuilder<'u> {
 	pub fn async_connect_insecure(self, handle: &Handle) -> async::ClientNew<async::TcpStream> {
 		let tcp_stream = match self.async_tcpstream(Some(false), handle) {
 			Ok(t) => t,
-			Err(e) => return future::err(e).boxed(),
+			Err(e) => return Box::new(future::err(e)),
 		};
 
 		let builder = ClientBuilder {
